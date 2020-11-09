1978e822eb67981216a2802f870d340251eb6a86
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2020-11-09 01:00:16 -0700

    Button Strikethrough on tap with strikethrough

--- app/javascript/components/Task.js
+++ app/javascript/components/Task.js
@@ -1,10 +1,23 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Style from "../styles/style.css"
+
 class Task extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.taskComplete = this.taskComplete.bind(this)
+  }
+
+  taskComplete(event) {
+    const element = event.target;
+    element.classList.toggle("crossed-line");
+  }
+
   render () {
     return (
       <React.Fragment>
-        <button>{this.props.taskName}</button><br/>
+        <button onClick={this.taskComplete}>{this.props.taskName}</button><br/>
       </React.Fragment>
     );
   }
